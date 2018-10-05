f6ba04447807dbe3969febadb6fcd32f348c1914
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-05 22:18:03 +0700

    Update DictionaryCommandline.java

--- Successful/src/successful/DictionaryCommandline.java
+++ Successful/src/successful/DictionaryCommandline.java
@@ -10,5 +10,10 @@ package successful;
  * @author dell
  */
 public class DictionaryCommandline {
-    
+    void showAllWord() {
+		Dictionary a = new Dictionary();
+		System.out.println("| Anh" + "    " + "| Viet");
+		for( int i = 0; i < a.word.size(); i++)
+		System.out.println("| " + a.word.get(i).GetWord_target() + " |" + a.word.get(i).GetWord_explain());
+	}
 }
