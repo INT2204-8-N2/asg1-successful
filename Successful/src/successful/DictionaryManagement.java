5fd616170f8abd68c8795fe7129173273b847ebe
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-05 22:18:55 +0700

    Update DictionaryManagement.java

--- Successful/src/successful/DictionaryManagement.java
+++ Successful/src/successful/DictionaryManagement.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package successful;
-
+import java.util.*;
 /**
  *
  * @author dell
  */
 public class DictionaryManagement {
-    
+    Dictionary x = new Dictionary();
+	void insertFromCommandline() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Nhap so luong tu: ");
+		int n;
+		n = input.nextInt();
+		input.nextLine();
+		for( int i = 0; i < n; i++)
+		{
+			String Eng = input.nextLine();
+			String Vie = input.nextLine();
+			Word w = new Word(Eng,Vie);
+			x.word.add(w);
+		}
+	}
 }
