c8fd2cc883278f5cc7232bcf53d26835016d509e
trinhanh268 <43316046+trinhanh268@users.noreply.github.com>
2018-10-07 14:34:57 +0700

    Update DictionaryManagement.java

--- Successful/src/successful/DictionaryManagement.java
+++ Successful/src/successful/DictionaryManagement.java
@@ -25,4 +25,36 @@ public class DictionaryManagement {
 			x.word.add(w);
 		}
 	}
+	void insertFromFile(){
+            try{
+                BufferedReader bu = new BufferedReader(new FileReader("dictionary.txt"));
+                bu.readLine();
+                String line = "";
+                for(int i = 0; (line = bu.readLine()) != null; i++ )
+                {
+                    Word w;
+                    w = new Word();
+                    String a[];
+                    a = line.split("\t");
+                    w.word_target = a[0];
+                    w.word_explain = a[1];
+                    x.word.add(w);
+                }
+                bu.close();
+                
+            }
+            catch (IOException e) {
+            e.printStackTrace();
+        }       
+        }
+        void dictionaryLookup(){
+        Dictionary x = new Dictionary();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap tu can tra: ");
+        String find_word = input.nextLine();
+        for(int i=0; i<x.word.size(); i++){
+            if(x.word.get(i).GetWord_target().equals(find_word))
+                System.out.println(x.word.get(i).GetWord_explain());
+        }
+    }
 }
